97991defbd96fbbae36634898cf65cce12ec81d3
unselchr <cdojrf@gmail.com>
2019-02-07 07:37:08 -0600

    downgraded terser to 3.14 per stackoverflow recomendation

---
+++ client/build/precache-manifest.40409434d1f6472b001f4624d6b25baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c631618db12d32288590",
+    "url": "/static/js/main.c631618d.chunk.js"
+  },
+  {
+    "revision": "d747e25aec042a542567",
+    "url": "/static/js/1.d747e25a.chunk.js"
+  },
+  {
+    "revision": "c631618db12d32288590",
+    "url": "/static/css/main.ed75f247.chunk.css"
+  },
+  {
+    "revision": "cfcf080138288c8246b98bb696c2ecf9",
+    "url": "/index.html"
+  }
+];
